45577ee1b5d3d3d4236612dd85ba2ee57fd0ada1
zcy <ciyi>
2018-12-14 16:33:55 +0800

    跳转

--- src/app/pages/childmodule/childmodule.component.ts
+++ src/app/pages/childmodule/childmodule.component.ts
@@ -1,14 +1,24 @@
 
 import { Component } from '@angular/core';
-
+import { Router, ActivatedRoute } from '@angular/router';
 @Component({
     selector: 'app-module',
     template: `
-      <div>
+      <div (click)="goto()">
       module
       </div>
     `,
   })
 
 export class ChildModuleComponent {
+    constructor(
+    private router: Router,
+    private route :ActivatedRoute
+    ) {
+           
+    }
+    goto(){
+        this.router.navigateByUrl('/child')
+    }
+    
 }
